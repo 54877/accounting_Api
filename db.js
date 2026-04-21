9d76890f85f7761fe9d89a11c6137b593d955f11
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-04-21 14:51:35 +0800

    修改env參數

--- db.js
+++ db.js
@@ -5,7 +5,6 @@ import "dotenv/config";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  family: 4,
 });
 
 export const query = (text, params) => pool.query(text, params);

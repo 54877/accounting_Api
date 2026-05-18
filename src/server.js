6831c050d3b49799f10b8662d92fb3807dc1b051
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-05-18 17:09:30 +0800

    新增dotenv

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
 import app from "./app.js";
 // 啟動伺服器
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`伺服器已啟動：http://localhost:${PORT}`);

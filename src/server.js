b9895c4f456325812f5b363fb091144e82d28392
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-05-25 11:34:40 +0800

    修改dotenv位置

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
+dotenv.config(); // 有jwt驗證就需要這段
 import app from "./app.js";
 // 啟動伺服器
-dotenv.config(); // 有jwt驗證就需要這段
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`伺服器已啟動：http://localhost:${PORT}`);
